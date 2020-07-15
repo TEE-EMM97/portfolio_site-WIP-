d7f8819142422e76da0902af18ef9173f48bc82b
TEE-EMM97 <36580928+TEE-EMM97@users.noreply.github.com>
2020-07-15 19:52:52 +0100

    LOL

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,14 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from "react"
+import { ThemeProvider } from "emotion-theming"
+import GlobalStateProvider from "./src/store/GlobalStateProvider"
+import { theme } from "./src/theme/theme"
+
+export const wrapRootElement = ({ element }) => (
+  <GlobalStateProvider>
+    <ThemeProvider theme={theme}>{element}</ThemeProvider>
+  </GlobalStateProvider>
+)
